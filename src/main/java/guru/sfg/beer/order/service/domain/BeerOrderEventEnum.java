b9a62b5aa3546c4f1c2f55afa7816491e0052004
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2020-08-14 18:55:10 +0200

    Add Enumerations for Spring State Machine - closes #15

---
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
@@ -0,0 +1,10 @@
+package guru.sfg.beer.order.service.domain;
+
+/**
+ * Created by jantonio on 14/08/2020.
+ */
+public enum BeerOrderEventEnum {
+    VALIDATE_ERROR, VALIDATION_PASSED, VALIDATION_FAILED,
+    ALLOCATION_SUCCESS, ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED,
+    BEERORDER_PICKED_UP
+}
